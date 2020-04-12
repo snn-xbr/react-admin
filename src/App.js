b9841211d85200d3b6bdd1c7a8b1d84e46c17987
snn-xbr <xbr_jiangsu@163.com>
2020-04-12 17:50:43 +0800

    基本配置1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 // 应用的根组件
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import Admin from './views/admins/admin';
+import Login from './views/login/login';
 
-export default class App extends Component{
-  render(){
+export default class App extends Component {
+  render() {
     return (
-      <div>App</div>
+      <Router>
+        <Switch>/*多个符合时，匹配其中一个，Route的顺序也有关*/
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ src/views/login/login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../../assets/login.less'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className="login">
+                <div className="login-header">Header</div>
+                <div className="login-content">Content</div>
+            </div>
+        )
+    }
+}
